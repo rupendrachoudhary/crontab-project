63d1e8911e926c8e7e316c1830a0ef35ebe2f276
rupendrachoudhary <rupendrachoudhary1990@gmail.com>
2022-08-28 22:22:38 +0530

    latest commit

--- cronapp/urls.py
+++ cronapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="cronapp-home"),
+    path('blog/', views.blog, name="cronapp-blog"),
+    path('contact/',views.contact, name="cronapp-contact")
 
 ]
--- cronapp/views.py
+++ cronapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, "cronapp/home.html")
+
+def blog(request):
+	return render(request, 'cronapp/blog.html')
+
+def contact(request):
+	return render(request, 'cronapp/contact.html')
