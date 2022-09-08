fec19bb9a7f8794d136b13ca5a2b5916d48af325
rupendrachoudhary <rupendrachoudhary1990@gmail.com>
2022-09-08 15:53:39 +0530

    django login 1

--- cronapp/urls.py
+++ cronapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import HomeView
 
 urlpatterns = [
-    path('',views.home, name="cronapp-home"),
-    path('blog/', views.blog, name="cronapp-blog"),
-    path('contact/',views.contact, name="cronapp-contact")
-
+    path('',HomeView.as_view(), name="home"),
+    path('blog/', views.blog, name="blog"),
+    path('contact/',views.contact, name="contact"),
+    
+   
 ]
--- cronapp/views.py
+++ cronapp/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+	template_name = 'cronapp/home.html'
+
+
+	def cronoutput(self, request, *args, **kwargs):
+		mins = request.POST.get('cron_mins')
+		hours = request.POST.get('cron_hours')  
+		days = request.POST.get('cron_days')
+		months = request.POST.get('cron_months')
+		weeks = request.POST.get('cron_weeks')
+
+		context = {
+		'mins':mins,
+		'hours':hours,
+		'days':days,
+		'months':months,
+		'weeks':weeks,
+
+		}
+		
+		print("hello")
+		print(f'{mins}{hours}{days}{months}{weeks}')
+		return render (request, self.template_name, context)
 
 
 
-def home(request):
-	return render(request, "cronapp/home.html")
+     
+
 
 def blog(request):
 	return render(request, 'cronapp/blog.html')
